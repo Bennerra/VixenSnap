dadd22dff6fb8910e1d9b36e388f492b695f1478
Nastya <elgina.anasta@yandex.ru>
2023-11-19 14:01:34 +0300

    Added a dark skeleton theme

--- src/modules/CardsSkeleton/CardSkeleton.tsx
+++ src/modules/CardsSkeleton/CardSkeleton.tsx
@@ -1,20 +1,24 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import classNames from "classnames/bind";
 import ContentLoader from "react-content-loader";
+
+import { ThemeContext } from "@/context";
 
 import styles from "./styles.module.scss";
 
 const cx = classNames.bind(styles);
 
 const CardSkeleton: FC = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <div className={cx("card-skeleton")}>
       <ContentLoader
         speed={2}
         width="100%"
         height="100%"
-        backgroundColor="#f3f3f3"
-        foregroundColor="#ecebeb"
+        backgroundColor={theme === "light" ? "#f3f3f3" : "#5a5a5a"}
+        foregroundColor={theme === "light" ? "#ecebeb" : "#535353"}
       >
         <rect x="0" y="0" rx="7" ry="7" width="100%" height="100%" />
       </ContentLoader>
